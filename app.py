f8a5a8d589b2d37151b88f169de5d85199da264b
FawazKh <80418395+FawazKh@users.noreply.github.com>
2023-02-04 12:37:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from soundproof import launch_website
 #this app is launched by gunicorn with given number of workers and threads from startup.txt
 app = launch_website()
 
-if __name__ == '__main__':
-    app.run(debug=True, ssl_context='adhoc')
+#if __name__ == '__main__':
+#    app.run(debug=True, ssl_context='adhoc')
